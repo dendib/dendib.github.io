8581ff832af70dcce755296248811a3d272b7783
Dendi <dendib888@gmail.com>
2025-06-29 02:57:09 +0300

    why

--- src/layout/main/MainContent.tsx
+++ src/layout/main/MainContent.tsx
@@ -26,7 +26,7 @@ const MainContent = () => {
             <div className="main-content-title">
               <div className="title-h1">
                   <h1 className="main-content-title-h1">
-                  Сервисные центры техники в Киеве222
+                  Сервисные центры техники в Киеве22222
                 </h1>
               </div>
               <div className="subtitle-box">
@@ -36,22 +36,21 @@ const MainContent = () => {
               </div>
               <div className="system_items">
                 <div className="system_item">
-                  <img className="img1" src="./src/assets/images/logotips/Frame 20.png" alt="logo" />
+                  <img className="img1" src="src/assets/images/logotips/Frame 20.png" alt="logo" />
                   <span>
                     <p>100% проверенные
                     сервисные центры</p>
                   </span>
                 </div>
                 <div className="system_item">
-                  <img className="img2" src="./src/assets/images/logotips/Vector-1.png" alt="logo" />
+                  <img className="img2" src="src/assets/images/logotips/Vector-1.png" alt="logo" />
                   <span>
                     <p>Квалифицированные
                     специалисты</p>
                   </span>
                 </div>
                 <div className="system_item">
-                    <img className="img3" src="./src/assets/images/logotips/Vector.png" alt="logo" />
-                    <img className="img3" src="./" alt="logo" />
+                    <img className="img3" src="src/assets/images/logotips/Vector.png" alt="logo" />
                   <span>
                     <p>Собственная
                     система рейтинга</p>
