265c8d6eea203ff65c4643e2f16369059d5b76fb
Dendi <dendib888@gmail.com>
2025-06-29 03:31:19 +0300

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'src/assets/styles/index.scss'
+import './assets/styles/index.scss';
 import App from './App.tsx'
 
 
